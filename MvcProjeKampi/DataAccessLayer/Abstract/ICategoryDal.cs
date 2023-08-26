2f54fb663550377d3c33f1c6954c58707114f32b
Mehmet <mehmet-acar-gs@hotmail.com>
2023-08-26 18:15:36 +0300

    First update

--- MvcProjeKampi/DataAccessLayer/Abstract/ICategoryDal.cs
+++ MvcProjeKampi/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,11 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal:IRepository<Category>
     {
-        List<Category> List();
-        void Insert(Category p);
-        void Update(Category p);
-        void Delete(Category p);
+        
     }
 }
